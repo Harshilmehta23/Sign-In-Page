f3fd890d99278f3fbfb0c180c097cae7295a1288
Harshilmehta23 <mharshil60@gmail.com>
2021-02-18 13:55:53 +0530

    Add Dark Mode Toggle to actually work

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,19 +1,24 @@
 import React, { Component, createContext } from 'react';
 
-const ThemeContext = createContext();
+export const ThemeContext = createContext();
 
-class ThemeProvider extends Component {
+export class ThemeProvider extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isDarkMode: true,
     };
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
   }
 
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
